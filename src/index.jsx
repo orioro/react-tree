ba74d6262c081c079db4a8c3cdb43d0815617e43
Simon Fan <simon@orioro.design>
2022-09-23 18:53:42 -0300

    refactor: apply prettier style standard

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,23 @@ import React, { createContext, useContext } from 'react'
 import PropTypes from 'prop-types'
 
 export const DEFAULT_TREE_CONTEXT = {
-  getId: node => node.id,
-  getChildNodes: node => node.childNodes,
+  getId: (node) => node.id,
+  getChildNodes: (node) => node.childNodes,
   renderLeaf: ({ id }, { level }) => (
     <div className='TreeNode TreeNode--Leaf'>
-      <div className='TreeNode__Label'>{id} ({ level })</div>
+      <div className='TreeNode__Label'>
+        {id} ({level})
+      </div>
     </div>
   ),
   renderBranch: ({ id }, { children, level }) => (
     <div className='TreeNode TreeNode--Branch'>
-      <div className='TreeNode__Label'>{id} ({ level })</div>
+      <div className='TreeNode__Label'>
+        {id} ({level})
+      </div>
       <div className='TreeNode__ChildNodeContainer'>{children}</div>
     </div>
-  )
+  ),
 }
 
 export const TreeContext = createContext(DEFAULT_TREE_CONTEXT)
@@ -22,30 +26,27 @@ export const TreeContext = createContext(DEFAULT_TREE_CONTEXT)
 export const TreeNodeList = ({ nodes, level = 0 }) => {
   const { getId } = useContext(TreeContext)
 
-  return <React.Fragment>
-    {nodes.map((node, index, nodes) => (
-      <TreeNode
-        key={getId(node)}
-        node={node}
-        level={level}
-        index={index}
-        nodes={nodes}
-      />
-    ))}
-  </React.Fragment>
+  return (
+    <React.Fragment>
+      {nodes.map((node, index, nodes) => (
+        <TreeNode
+          key={getId(node)}
+          node={node}
+          level={level}
+          index={index}
+          nodes={nodes}
+        />
+      ))}
+    </React.Fragment>
+  )
 }
 TreeNodeList.displayName = 'TreeNodeList'
 TreeNodeList.propTypes = {
-  nodes: PropTypes.array.isRequired
+  nodes: PropTypes.array.isRequired,
 }
 
-
 export const TreeNode = ({ node, level = 0, index, nodes }) => {
-  const {
-    getChildNodes,
-    renderLeaf,
-    renderBranch,
-  } = useContext(TreeContext)
+  const { getChildNodes, renderLeaf, renderBranch } = useContext(TreeContext)
   const childNodes = getChildNodes(node)
 
   return Array.isArray(childNodes)
@@ -53,10 +54,7 @@ export const TreeNode = ({ node, level = 0, index, nodes }) => {
         level,
         index,
         nodes,
-        children: <TreeNodeList
-          nodes={childNodes}
-          level={level + 1}
-        />
+        children: <TreeNodeList nodes={childNodes} level={level + 1} />,
       })
     : renderLeaf(node, {
         level,
@@ -66,5 +64,5 @@ export const TreeNode = ({ node, level = 0, index, nodes }) => {
 }
 TreeNode.displayName = 'TreeNode'
 TreeNode.propTypes = {
-  node: PropTypes.object.isRequired
+  node: PropTypes.object.isRequired,
 }
--- src/index.stories.jsx
+++ src/index.stories.jsx
@@ -1,13 +1,12 @@
-import React, { useState, memo } from 'react';
+import React, { useState, memo } from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
 
 import { TreeNodeList, TreeContext } from './index'
 
 const App = () => {
-
   const treeNodes = [
     {
       id: 'node1',
@@ -22,22 +21,21 @@ const App = () => {
               id: 'node112',
             },
             {
-              id: 'node113'
-            }
-          ]
+              id: 'node113',
+            },
+          ],
         },
         {
-          id: 'node12'
-        }
-      ]
+          id: 'node12',
+        },
+      ],
     },
     {
-      id: 'node2'
-    }
+      id: 'node2',
+    },
   ]
 
   return <TreeNodeList nodes={treeNodes} />
 }
 
-storiesOf('Tree', module)
-  .add('with text', () => <App />)
+storiesOf('Tree', module).add('with text', () => <App />)
